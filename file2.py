22997b3f1779e468e49800539e8d1bb8d2ede992
Sakshi.M <01fe24bca258@kletech.ac.in>
2025-11-03 14:11:57 +0530

    save

--- file2.py
+++ file2.py
@@ -6,9 +6,5 @@ time = 3            # Time in years
 # Calculate Simple Interest
 simple_interest = (principal * rate * time) / 100
 
-# Display the result
-print("----- Simple Interest Calculator -----")
-print(f"Principal Amount: ₹{principal}")
-print(f"Rate of Interest: {rate}%")
-print(f"Time Period: {time} years")
+
 print(f"Calculated Simple Interest: ₹{simple_interest}")
